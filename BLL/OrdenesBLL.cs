4fb33856b3492e05aea3077c8bf73162ceb06482
AdrianMendezVargas <velociraptor088@gmail.com>
2020-03-09 14:21:03 -0400

    Restando o Sumando al inventario correspondiente al producto en caso de que se modifique la lista de productos de una orden

--- BLL/OrdenesBLL.cs
+++ BLL/OrdenesBLL.cs
@@ -4,110 +4,120 @@ using RegistroOrdenes.Entidades;
 using System.Linq;
 
 namespace RegistroOrdenes.BLL {
-    public class OrdenesBLL {
+	public class OrdenesBLL {
 
-        public static Orden Buscar(int OrdenId) {
-            Contexto db = new Contexto();
+		public static Orden Buscar(int OrdenId) {
+			Contexto db = new Contexto();
 
-            var Orden = db.Ordenes.Where(o => o.OrdenId == OrdenId)
-                              .Include(o => o.DetalleProductos)
-                              .SingleOrDefault();
+			var Orden = db.Ordenes.Where(o => o.OrdenId == OrdenId)
+							  .Include(o => o.DetalleProductos)
+							  .SingleOrDefault();
 
-            return Orden;
-        }
+			return Orden;
+		}
 
-        public static bool Guardar(Orden orden) {    //TODO: Restar la cantidad de productos vendidos al inventario
-            bool paso = false;
+		public static bool Guardar(Orden orden) {    
+			bool paso = false;
 
-            Contexto db = new Contexto();
+			Contexto db = new Contexto();
 
-            try {
+			try {
 
-                db.Ordenes.Add(orden);
+				db.Ordenes.Add(orden);
 
-                paso = db.SaveChanges() > 0;
+				paso = db.SaveChanges() > 0;
 
-                if (paso) {
-                    foreach (var item in orden.DetalleProductos) {
-                        Producto producto = ProductosBLL.Buscar(item.ProductoId);
-                        producto.CantidadInventario--;
-                        ProductosBLL.Modificar(producto);
-                    }
-                }
+				if (paso) {
+					foreach (var item in orden.DetalleProductos) {
+						Producto producto = ProductosBLL.Buscar(item.ProductoId);
+						producto.CantidadInventario--;
+						ProductosBLL.Modificar(producto);
+					}
+				}
 
-            } catch (System.Exception) {
+			} catch (System.Exception) {
 
-                throw;
+				throw;
 
-            } finally {
-                db.Dispose();
-            }
+			} finally {
+				db.Dispose();
+			}
 
-            return paso;
-        }
+			return paso;
+		}
 
-        public static bool Modificar(Orden orden) {
-            bool paso = false;
+		public static bool Modificar(Orden orden) {
+			bool paso = false;
 
             //buscar las entidades que no estan para removerlas. 
             var Anterior = Buscar(orden.OrdenId);
 
-            Contexto db = new Contexto();
-            foreach (var item in Anterior.DetalleProductos) {
+			Contexto db = new Contexto();
+			foreach (var item in Anterior.DetalleProductos) {
 
-                if (!orden.DetalleProductos.Exists(d => d.OrdenDetalleId == item.OrdenDetalleId)) {
-                    db.Entry(item).State = EntityState.Deleted;
-                }
-            }    
+				if (!orden.DetalleProductos.Exists(d => d.OrdenDetalleId == item.OrdenDetalleId)) {
+					Producto producto = ProductosBLL.Buscar(item.ProductoId);
+					producto.CantidadInventario++;        //Devolviendo el producto eliminado al inventario
+					ProductosBLL.Modificar(producto);
 
-            //recorrer el detalle
-            foreach (var item in orden.DetalleProductos) {
+					db.Entry(item).State = EntityState.Deleted;
+				}
+			}    
+
+			//recorrer el detalle para verificar si se agrego o se modifico un producto
+			foreach (var item in orden.DetalleProductos) {
 
                 //Muy importante indicar que pasara con la entidad del detalle
                 var estado = item.OrdenDetalleId > 0 ? EntityState.Modified : EntityState.Added;
-                db.Entry(item).State = estado;
-            }  
+				db.Entry(item).State = estado;
 
-            try {
-                //Indicar que se esta modificando el encabezado
-                db.Entry(orden).State = EntityState.Modified;
-                paso = db.SaveChanges() > 0;
+				if (estado == EntityState.Added) {
+					Producto producto = ProductosBLL.Buscar(item.ProductoId);
+					producto.CantidadInventario--;        //Restando el producto agregado al inventario
+					ProductosBLL.Modificar(producto);
+				}
+			}  
 
-            } catch (System.Exception) {
+			try {
+				//Indicar que se esta modificando el encabezado
+				db.Entry(orden).State = EntityState.Modified;
+				paso = db.SaveChanges() > 0;
 
-                throw;
+			} catch (System.Exception) {
 
-            } finally {
-                db.Dispose();
-            }
+				throw;
 
-            return paso;
-        }
+			} finally {
+				db.Dispose();
+			}
 
-        public static bool Eliminar(int OrdenId) {
-            bool paso = false;
+			return paso;
+		}
 
-            Contexto db = new Contexto();
+		public static bool Eliminar(int OrdenId) {
+			bool paso = false;
 
-            try {
+			Contexto db = new Contexto();
 
-                var ordenEliminada = Buscar(OrdenId);
-                db.Entry(ordenEliminada).State = EntityState.Deleted;
-                paso = (db.SaveChanges() > 0);
+			try {
 
-            } catch (System.Exception) {
+				var ordenEliminada = Buscar(OrdenId);
+				db.Entry(ordenEliminada).State = EntityState.Deleted;
+				paso = (db.SaveChanges() > 0);
 
-                throw;
+			} catch (System.Exception) {
 
-            } finally {
-                db.Dispose();
-            }
+				throw;
 
-            return paso;
+			} finally {
+				db.Dispose();
+			}
 
-        }
+			return paso;
 
-    }
+		}
+
+	}
 }
 
 
